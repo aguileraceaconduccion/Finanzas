bdddbe21ed27b17c835178a2a8e94c211b124210
aguileraceaconduccion <aguileraceaconduccion@gmail.com>
2026-07-11 20:05:01 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "finanzas-pro-v4";
+const ASSETS = [
+  "index.html",
+  "manifest.json",
+  "appicon.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
